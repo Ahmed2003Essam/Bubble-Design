e661047caaf294080170fe7adffb458f33bee24f
Ahmed Essam Lotfi <63603118+Ahmed2003Essam@users.noreply.github.com>
2021-11-04 19:38:57 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/* Start Typer*/
+$('document').ready(function(){
+    var typed = new Typed('.type', {
+      strings: ['Welcome  to my Relaxing world','Breathe the freedom', 'Live like fish'],
+      typeSpeed: 200, // to assign speed value in going foward
+      loop: true, // to make it in an infinte loop
+      backspeed: 100 // to give speed value in returing back
+    
+    });
+  });
+/*End Typer*/  
